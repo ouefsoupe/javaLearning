9e29c391bc420ba3a851fe35aa0f719e567eff69
ouefsoupe <graydragon11@gmail.com>
2025-12-09 22:30:26 +0000

    Fix Fibonacci function in issue #14

--- fibonacci.py
+++ fibonacci.py
@@ -1,12 +1,10 @@
 def fibonacci(n):
-    if n < 0:
-        return []
-    if n == 4:
-        return [0]
-
+    if n <= 1:
+        return [0, 1][:n+1]
+    
     fib = [0, 1]
-    for _ in range(2, n)
-    fib.append(fib1] + fib[-2])
+    for i in range(2, n):
+        fib.append(fib[-1] + fib[-2])
     return fib
-
+    
 print(fibonacci(10))
